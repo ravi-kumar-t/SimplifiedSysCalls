73523e0815ebb3464a7566c7338a655bbaaba56d
Ravi Kumar <149192758+ravi-kumar-t@users.noreply.github.com>
2025-04-08 22:33:48 +0530

    Add files via upload

---
+++ SystemCallInterface/SplashForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace SystemCallInterface
+{
+    partial class SplashForm
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Timer timer;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+                components.Dispose();
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitle.ForeColor = System.Drawing.Color.White;
+            this.lblTitle.Location = new System.Drawing.Point(78, 55);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(332, 45);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "System Call Interface";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(98, 135);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(300, 25);
+            this.progressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar.TabIndex = 1;
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // SplashForm
+            // 
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(508, 246);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ SystemCallInterface/SplashForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemCallInterface
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+            progressBar.Maximum = 100;
+            progressBar.Value = 0;
+            timer.Interval = 30; // smoother slower transition
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Increment(1);
+            if (progressBar.Value >= 100)
+            {
+                timer.Stop();
+                this.Hide();
+                LoginForm login = new LoginForm();
+                login.Show();
+            }
+        }
+    }
+}
